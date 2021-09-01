a28eee95a2b57413dadcc98b94892c36a4fc61e1
Zain <saeedzain20@gmail.com>
2021-09-01 14:00:53 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Operators operators = new Operators();
+//        operators.divisionPractice();
+//        operators.incrementDecrement();
+
+    }
+
+
+
+    //concat function String
+    public static void concatFunctionString(){
+        // create first string
+        String first = "Java ";
+        System.out.println("First String: " + first);
+
+        // create second
+        String second = "Programming";
+        System.out.println("Second String: " + second);
+
+        // join two strings
+        String joinedString = first.concat(second);
+        System.out.println("Joined String: " + joinedString);
+    }
+    //equals function String
+    public static void equalsFunctionString()
+    {
+        String first = "Java Programming";
+        String second = "Java Programming";
+        String third = "Python Programming";
+
+        System.out.println("Comparing 1st and 2nd: " + first.equals(second));
+        System.out.println("Comparing 2nd and 3rd: " + second.equals(third));
+    }
+
+
+
+
+
+    //Java Program to Print an Integer (Entered by the User)
+//    public void printInteger(){
+//        System.out.println("Enter an integer: ");
+//
+//    }
+}
---
+++ src/com/company/Operators.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Operators {
+    //Arithmetic Operations Practice
+    public void divisionPractice()
+    {
+        //(9.0 / 2) is 4.5
+        float a = 9.0f;
+        int b = 2;
+        //it will give floating number
+        System.out.println(a/b);
+    }
+
+    //increment decrement
+    public void incrementDecrement(){
+        int num = 5;
+        int num2 = 5;
+        System.out.println(num++);
+        System.out.println(num);
+        System.out.println(++num2);
+        System.out.println(num2);
+    }
+}
